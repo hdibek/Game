21eecf7da419087c3965d5b9957280c11dce2e80
Hannah <dibek2810@students.d211.org>
2018-04-29 20:14:09 -0500

    able to click on and detect which item is clicked when am item is clicked checkmark will appear

--- javascript.js
+++ javascript.js
@@ -1,180 +1,169 @@
 var Title = new Image();
 Title.src = "resources/Title.PNG";
+var checkmark = new Image();
+checkmark.src = "resources/checkmark.png";
 
-var airplane = new Image();
-airplane.src = "resources/airplane.jpg";
+function addImage(src, x,y, width, height){
+    var image = new Image();
+    image.src = src;
+    return {
+        image: image,
+        x:x,
+        y:y,
+        width:width,
+        height:height
+    }
+}
+
 var beach = new Image();
 beach.src = "resources/beach.jpg";
-var beachball = new Image();
-beachball.src = "resources/beachball.png";
-var beachchair = new Image();
-beachchair.src = "resources/beachchair.png";
-var dolphin = new Image();
-dolphin.src = "resources/dolphin.png";
-var floaty = new Image();
-floaty.src = "resources/floaty.png";
-var sailboat = new Image();
-sailboat.src = "resources/sailboat.png";
-var soccerball = new Image();
-soccerball.src = "resources/soccerball.png";
-var speedboat = new Image();
-speedboat.src = "resources/speedboat.png";
-var towel = new Image();
-towel.src = "resources/towel.png";
-var umbrella = new Image();
-umbrella.src = "resources/umbrella.png";
+var beachImages = [
+    addImage("resources/sailboat.png",60,265,47,41),
+    addImage("resources/speedboat.png",30,330,66,25),
+    addImage("resources/beachball.png",525,375,30,29),
+    addImage("resources/towel.png",390,415,60,22),
+    addImage("resources/umbrella.png",415,375,64,58),
+    addImage("resources/dolphin.png",265,265,30,30),
+    addImage("resources/floaty.png",250,350,42,26),
+    addImage("resources/airplane.jpg",50,50,71,35),
+    addImage("resources/beachchair.png",430,410,30,35),
+    addImage("resources/soccerball.png",299,422,15,15)
+];
 
-var blueflower = new Image();
-blueflower.src = "resources/blueflower.png";
-var dog = new Image();
-dog.src = "resources/dog.jpg";
-var doghouse = new Image();
-doghouse.src = "resources/doghouse.png";
 var garden = new Image();
 garden.src = "resources/garden.jpg";
-var gloves = new Image();
-gloves.src = "resources/gloves.png";
-var pinkpottedflower = new Image();
-pinkpottedflower.src = "resources/pinkpottedflower.png";
-var pottedflowers = new Image();
-pottedflowers.src = "resources/pottedflowers.png";
-var sunflowers = new Image();
-sunflowers.src = "resources/sunflowers.png";
-var tree = new Image();
-tree.src = "resources/tree.png";
-var wateringcan = new Image();
-wateringcan.src = "resources/wateringcan.png";
-var wheelbarrow = new Image();
-wheelbarrow.src = "resources/wheelbarrow.png";
+var gardenImages = [
+    addImage("resources/tree.png",225,55,273,300),
+    addImage("resources/blueflower.png",150,265,16,32),
+    addImage("resources/dog.jpg",470,355,125,91),
+    addImage("resources/doghouse.png",450,250,132,120),
+    addImage("resources/gloves.png",25,420,38,30),
+    addImage("resources/pinkpottedflower.png",70,320,63,120),
+    addImage("resources/pottedflowers.png",180,235,67,80),
+    addImage("resources/sunflowers.png",490,130,44,60),
+    addImage("resources/wateringcan.png",70,260,69,57),
+    addImage("resources/wheelbarrow.png",130,300,136,68)
 
-var angelfish = new Image();
-angelfish.src = "resources/angelfish.png";
-var bluefish = new Image();
-bluefish.src = "resources/bluefish.png";
-var jellyfish = new Image();
-jellyfish.src = "resources/jellyfish.png";
-var seahorse = new Image();
-seahorse.src = "resources/seahorse.png";
-var seaturtle = new Image();
-seaturtle.src = "resources/seaturtle.png";
-var seaweed = new Image();
-seaweed.src = "resources/seaweed.png";
-var shell = new Image();
-shell.src = "resources/shell.png";
-var starfish = new Image();
-starfish.src = "resources/starfish.png";
-var submarine = new Image();
-submarine.src = "resources/submarine.png";
+];
+
 var underwater = new Image();
 underwater.src = "resources/underwater.png";
-var whale = new Image();
-whale.src = "resources/whale.png";
+var underwaterImages = [
+    addImage("resources/seaweed.png",250,375,90,60),
+    addImage("resources/bluefish.png",350,285,95,47),
+    addImage("resources/seahorse.png",550,350,30,55),
+    addImage("resources/starfish.png",225,425,38,20),
+    addImage("resources/submarine.png",60,80,120,120),
+    addImage("resources/whale.png",325,30,240,136),
+    addImage("resources/jellyfish.png",500,300,60,46),
+    addImage("resources/seaturtle.png",125,225,96,72),
+    addImage("resources/shell.png",175,375,60,37),
+    addImage("resources/angelfish.png",275,200,60,84)
+];
+
+var gameItems =[];
+
+function drawCanvasItems(allItems){
+    var ctx = document.getElementById("myCanvas").getContext("2d");
+    for(i=0;i<allItems.length;i++){
+        var img = allItems[i];
+        var image = img.image;
+        ctx.drawImage(image, img.x, img.y, img.width, img.height);
+    }
+}
+var selectedCanvas = 'none';
+
+function imageClicked(evt){
+    var whichItemClicked = detectItemClicked(evt.clientX, evt.clientY);
+
+    if(whichItemClicked!=null){
+        var ctx = document.getElementById("myCanvas").getContext("2d");
+        // alert("You clicked: " + whichItemClicked.image.src + " at: " + evt.clientX + "," + evt.clientY);
+        ctx.drawImage(checkmark,whichItemClicked.searchX+(whichItemClicked.searchWidth-15)/2,512, 30 , 36);
+    }
+}
+function detectItemClicked(x, y) {
+    if (selectedCanvas === 'none') {
+        return null;
+    }
+    else {
+        for (i = 0; i < gameItems.length; i++) {
+            var gameItem = gameItems[i];
+            if ((gameItem.x <= x) && (x <= (gameItem.x + gameItem.width))
+                && (gameItem.y <= y) && (y <= (gameItem.y + gameItem.height))) {
+                return gameItem;
+            }
+        }
+        return null;
+    }
+}
 
 function initialize() {
     var ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.drawImage(Title,0,0,600,600);
+    document.getElementById("myCanvas").addEventListener('click', imageClicked,false);
 }
-// var createImage = function(src, title,xcoord,ycoord,mover) {
-//     var img   = new Image();
-//     img.src   = src;
-//     img.alt   = title;
-//     img.title = title;
-//     img.left = xcoord;
-//     img.top = ycoord;
-//     img.move = mover;
-//     return img;
-// };
+
 function Beach(){
     var ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.fillStyle="#ffffff";
     ctx.fillRect(0,0,600,600);
     ctx.drawImage(beach,0,0,600,450);
-    ctx.drawImage(sailboat,60,265,47,41);
-    ctx.drawImage(speedboat,30,330,66,25);
-    ctx.drawImage(beachball,525,375,30,29);
-    ctx.drawImage(towel,390,415,60,22);
-    ctx.drawImage(umbrella,415,375,64,58);
-    ctx.drawImage(dolphin,265,265,30,30);
-    ctx.drawImage(floaty,250,350,42,26);
-    ctx.drawImage(airplane,50,50,71,35);
-    ctx.drawImage(beachchair,430,410,30,35);
-    ctx.drawImage(soccerball,299,422,15,15);
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "#000000";
-    ctx.fillText("Find These Items:",0,473);
-    playBeach();
+
+    selectedCanvas = 'beach';
+
+    drawCanvasItems(beachImages);
+    drawSearchItems(beachImages);
 }
 function Garden(){
     var ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.fillStyle="#ffffff";
     ctx.fillRect(0,0,600,600);
     ctx.drawImage(garden,0,0,600,450);
-    ctx.drawImage(tree,225,55,273,300);
-    ctx.drawImage(blueflower,150,265,16,32);
-    ctx.drawImage(dog,470,355,125,91);
-    ctx.drawImage(doghouse,450,250,132,120);
-    ctx.drawImage(gloves,25,420,38,30);
-    ctx.drawImage(pinkpottedflower,70,320,63,120);
-    ctx.drawImage(pottedflowers,180,235,67,80);
-    ctx.drawImage(sunflowers,490,130,44,60);
-    ctx.drawImage(wateringcan,70,260,69,57);
-    ctx.drawImage(wheelbarrow,130,300,136,68);
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "#000000";
-    ctx.fillText("Find These Items:",0,473);
-    playGarden();
+
+    selectedCanvas = 'garden';
+
+    drawCanvasItems(gardenImages);
+    drawSearchItems(gardenImages);
 }
 function Underwater(){
     var ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.fillStyle="#ffffff";
     ctx.fillRect(0,0,600,600);
+
+    selectedCanvas = 'underwater';
+
     ctx.drawImage(underwater,0,0,600,450);
-    ctx.drawImage(seaweed,250,375,90,60);
-    ctx.drawImage(bluefish,350,285,95,47);
-    ctx.drawImage(seahorse,550,350,30,55);
-    ctx.drawImage(starfish,225,425,38,20);
-    ctx.drawImage(submarine,60,80,120,120);
-    ctx.drawImage(whale,325,30,240,136);
-    ctx.drawImage(jellyfish,500,300,60,46);
-    ctx.drawImage(seaturtle,125,225,96,72);
-    ctx.drawImage(shell,175,375,60,37);
-    ctx.drawImage(angelfish,275,200,60,84);
+    drawCanvasItems(underwaterImages);
+    drawSearchItems(underwaterImages);
+}
+
+function drawSearchItems(allItems){
+    allItems.forEach(function(item){item.selected = false});//reset from previous game
+
+    gameItems = pickGameItems(allItems);
+    var ctx = document.getElementById("myCanvas").getContext("2d");
     ctx.font = "30px Arial";
     ctx.fillStyle = "#000000";
     ctx.fillText("Find These Items:",0,473);
-    playUnderwater();
-}
-
-function playBeach(){
-    var beachItems = [airplane , beachball , beachchair , dolphin , floaty , sailboat , soccerball , speedboat , towel , umbrella ];
-    playGame(beachItems);
-
-}
-function playGarden(){
-    var gardenItems = [blueflower , dog , doghouse , gloves , pinkpottedflower , pottedflowers , sunflowers , tree , wateringcan , wheelbarrow];
-    playGame(gardenItems);
-}
-function playUnderwater(){
-    var underwaterItems = [angelfish , bluefish , jellyfish , seahorse , seaturtle , seaweed , shell , starfish , submarine , whale];
-    playGame(underwaterItems);
-}
-function playGame(allItems){
-    var gameItems = pickGameItems(allItems);
-    var ctx = document.getElementById("myCanvas").getContext("2d");
     var left = 10;
     for(i=0;i<gameItems.length;i++) {
-        var proportions = gameItems[i].height/gameItems[i].width;
+        var image = gameItems[i].image;
+        var proportions = image.height/image.width;
         var width = 60/proportions;
-        ctx.drawImage(gameItems[i],left, 500, width, 60);
+        ctx.drawImage(image,left, 500, width, 60);
+        gameItems[i].searchX = left;
+        gameItems[i].searchWidth = width;
         left+= width+10;
     }
 }
 function pickGameItems(allItems) {
     var gameItems = [];
     for (i = 0; i < 5; i++) {
-        var bnum = Math.floor(Math.random() * allItems.length);
-        gameItems.push(allItems[bnum]);
-        allItems.splice(bnum,1);
+        var chooseItems = allItems.filter(function(item){return item.selected!==true});
+        var bnum = Math.floor(Math.random() * chooseItems.length);
+        gameItems.push(chooseItems[bnum]);
+        chooseItems[bnum].selected = true;
     }
     return gameItems;
 }
